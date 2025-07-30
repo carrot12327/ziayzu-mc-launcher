3bff89ad3f9471f95abd351afb035f6530809d08
carrot12327 <carrotmc5@gmail.com>
2025-07-30 14:38:46 +0530

    Update languagelist_updater.sh

--- scripts/languagelist_updater.sh
+++ scripts/languagelist_updater.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-THISDIR="$(dirname "$0")"
-LANGFILE="$THISDIR/../app_pojavlauncher/src/main/assets/language_list.txt"
-rm -f "$LANGFILE"
+THISDIR=`dirname $0`
+LANGFILE=$THISDIR/../app_pojavlauncher/src/main/assets/language_list.txt
 
-for dir in "$THISDIR"/../app_pojavlauncher/src/main/res/values-*; do
-    [ -d "$dir" ] && basename "$dir"
-done > "$LANGFILE"
+rm -f $LANGFILE
+echo $THISDIR/../app_pojavlauncher/src/main/res/values-* | xargs -- basename -a > $LANGFILE
